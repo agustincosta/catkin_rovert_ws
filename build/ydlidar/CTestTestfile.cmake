57c08e84e1d573a3fedb1a903d1c529e27a61462
SBernheim <santibernheim@gmail.com>
2019-04-12 10:24:53 -0300

    Acomodando la repo

--- build/ydlidar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/catkin_rovert_ws/src/ydlidar
-# Build directory: /home/ubuntu/catkin_rovert_ws/build/ydlidar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
